7906c4d7d5400a84be65a9105e5c33ca77e12bae
Green S <kaylasierrabartley@gmail.com>
2020-12-15 23:27:49 -0600

    made data show on html document

--- src/page-template.js
+++ src/page-template.js
@@ -1,5 +1,5 @@
 
-const generateManagerCards = array => {
+const generateManagerCard = array => {
   return `
     ${array.map(({name, id, email, number}) => {
       return`
@@ -12,6 +12,48 @@ const generateManagerCards = array => {
       <li class="list-group-item">ID: ${id}</li>
       <li class="list-group-item">Email: ${email}</li>
       <li class="list-group-item">Office Number: ${number}</li>
+    </ul>
+  </div>
+  `;
+  })
+  .join('')}
+  `;
+};
+
+const generateEngineerCards = array => {
+  return `
+    ${array.map(({name, id, email, username}) => {
+      return`
+  <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="..."> 
+    <div class="card-body">  
+      <h5 class="card-title">${name}</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${id}</li>
+      <li class="list-group-item">Email: ${email}</li>
+      <li class="list-group-item">GitHub Username: ${username}</li>
+    </ul>
+  </div>
+  `;
+  })
+  .join('')}
+  `;
+};
+
+const generateInternCards = array => {
+  return `
+    ${array.map(({name, id, email, school}) => {
+      return`
+  <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="..."> 
+    <div class="card-body">  
+      <h5 class="card-title">${name}</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${id}</li>
+      <li class="list-group-item">Email: ${email}</li>
+      <li class="list-group-item">Office Number: ${school}</li>
     </ul>
   </div>
   `;
@@ -34,7 +76,9 @@ module.exports = (managerArr, engineerArr, internArr) => {
     <link rel="stylesheet" href="./style.css">
   </head>
   <body>
-    ${generateManagerCards(managerArr)}
+    ${generateManagerCard(managerArr)}
+    ${generateEngineerCards(engineerArr)}
+    ${generateInternCards(internArr)}
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
